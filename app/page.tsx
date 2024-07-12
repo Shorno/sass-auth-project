41030b5f2fb7eebea1c4603053f6f9eebe42e847
Shorno <fb.shorno@gmail.com>
2024-07-12 08:57:23 +0600

    setup prisma/neon db

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import {Button} from "@/components/ui/button";
 import {LoginButton} from "@/components/auth/login-button";
 
 export default function Home() {
+
     return (
         <main className="flex h-full flex-col items-center justify-center bg-gradient-to-b from-sky-400 to-blue-800 ">
             <div className={"space-y-6 text-center"}>
