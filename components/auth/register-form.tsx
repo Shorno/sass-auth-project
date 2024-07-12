5103952ae6ae88d6d40ce57ef803714d48d37016
Shorno <fb.shorno@gmail.com>
2024-07-12 08:07:50 +0600

    login/ register page

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -1,7 +1,7 @@
 "use client"
 import CardWrapper from "@/components/auth/card-wrapper";
 import {useForm} from "react-hook-form";
-import {LoginSchema} from "@/schemas";
+import {LoginSchema, RegisterSchema} from "@/schemas";
 import {z} from "zod";
 import {zodResolver} from "@hookform/resolvers/zod";
 import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
@@ -9,30 +9,31 @@ import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
 import FormError from "@/components/ui/form-error";
 import FormSuccess from "@/components/ui/form-success";
-import {login} from "@/actions/login";
 import {useState, useTransition} from "react";
+import {register} from "@/actions/register";
 
-export default function LoginForm() {
+export default function RegisterForm() {
     const [error, setError] = useState<string | undefined>("")
     const [success, setSuccess] = useState<string | undefined>("")
     const [isPending, startTransition] = useTransition();
 
 
-    const form = useForm<z.infer<typeof LoginSchema>>({
-        resolver: zodResolver(LoginSchema),
+    const form = useForm<z.infer<typeof RegisterSchema>>({
+        resolver: zodResolver(RegisterSchema),
         defaultValues: {
             email: "",
             password: "",
+            name: "",
         }
     })
 
 
-    const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
         setError("");
         setSuccess("");
 
         startTransition(() => {
-            login(values)
+            register(values)
                 .then((data) => {
                     setError(data.error);
                     setSuccess(data.success);
@@ -44,9 +45,9 @@ export default function LoginForm() {
     return (
         <>
             <CardWrapper
-                headerLabel={"Welcome to Auth Service"}
-                backButtonLabel={"Don't have an account?"}
-                backButtonHref={"/auth/register"}
+                headerLabel={"Crete an account"}
+                backButtonLabel={"Already have an account?"}
+                backButtonHref={"/auth/login"}
                 showSocial
             >
                 <Form {...form}>
@@ -54,6 +55,23 @@ export default function LoginForm() {
                           className={"space-y-6"}
                     >
                         <div className={"space-y-4"}>
+
+
+                            <FormField
+                                control={form.control}
+                                name={"name"}
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel>Name</FormLabel>
+                                        <FormControl>
+                                            <Input disabled={isPending} {...field} type={"text"}
+                                                   placeholder={"Name"}/>
+                                        </FormControl>
+                                        <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
+
                             <FormField
                                 control={form.control}
                                 name={"email"}
@@ -86,7 +104,7 @@ export default function LoginForm() {
                         </div>
                         <FormError message={error}/>
                         <FormSuccess message={success}/>
-                        <Button disabled={isPending} type={"submit"} className={"w-full"}>Login</Button>
+                        <Button disabled={isPending} type={"submit"} className={"w-full"}>Crete an account</Button>
                     </form>
                 </Form>
             </CardWrapper>
